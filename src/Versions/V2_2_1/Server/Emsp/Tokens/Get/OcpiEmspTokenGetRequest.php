cfb74df18bf4c0729268efd21428410069db300b
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-03-06 03:31:48 +0100

    Locations GetListingRequest

--- src/Versions/V2_2_1/Server/Emsp/Tokens/Get/OcpiEmspTokenGetRequest.php
+++ src/Versions/V2_2_1/Server/Emsp/Tokens/Get/OcpiEmspTokenGetRequest.php
@@ -4,45 +4,8 @@ declare(strict_types=1);
 
 namespace Chargemap\OCPI\Versions\V2_2_1\Server\Emsp\Tokens\Get;
 
-use Chargemap\OCPI\Common\Server\OcpiListingRequest;
-use DateTime;
-use Exception;
-use InvalidArgumentException;
-use Psr\Http\Message\ServerRequestInterface;
+use Chargemap\OCPI\Versions\V2_2_1\Server\Common\OcpiPaginatedRequest;
 
-class OcpiEmspTokenGetRequest extends OcpiListingRequest
+class OcpiEmspTokenGetRequest extends OcpiPaginatedRequest
 {
-    private ?DateTime $dateFrom;
-
-    private ?DateTime $dateTo;
-
-    /**
-     * @param ServerRequestInterface $request
-     * @throws Exception
-     */
-    public function __construct(ServerRequestInterface $request)
-    {
-        $params = $request->getQueryParams();
-
-        $dateFrom = array_key_exists('date_from', $params) ? new DateTime($params['date_from']) : null;
-        $dateTo = array_key_exists('date_to', $params) ? new DateTime($params['date_to']) : null;
-
-        if ($dateFrom !== null && $dateTo !== null && $dateFrom > $dateTo) {
-            throw new InvalidArgumentException(sprintf('Date ranges (from %s to %s) are not valid.', $dateFrom->format(DATE_ATOM), $dateTo->format(DATE_ATOM)));
-        }
-
-        parent::__construct($request);
-        $this->dateFrom = $dateFrom;
-        $this->dateTo = $dateTo;
-    }
-
-    public function getDateFrom(): ?DateTime
-    {
-        return $this->dateFrom;
-    }
-
-    public function getDateTo(): ?DateTime
-    {
-        return $this->dateTo;
-    }
 }
