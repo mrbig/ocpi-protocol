1c6ddeae88c58049286f146af7062b3b40e29f27
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-05-14 23:04:34 +0200

    New connector types added

--- src/Versions/V2_2_1/Common/Models/ConnectorType.php
+++ src/Versions/V2_2_1/Common/Models/ConnectorType.php
@@ -20,6 +20,11 @@ use MyCLabs\Enum\Enum;
  * @method static self DOMESTIC_J()
  * @method static self DOMESTIC_K()
  * @method static self DOMESTIC_L()
+ * @method static self DOMESTIC_M()
+ * @method static self DOMESTIC_N()
+ * @method static self DOMESTIC_O()
+ * @method static self GBT_AC()
+ * @method static self GBT_DC()
  * @method static self IEC_60309_2_SINGLE_16()
  * @method static self IEC_60309_2_THREE_16()
  * @method static self IEC_60309_2_THREE_32()
@@ -30,6 +35,14 @@ use MyCLabs\Enum\Enum;
  * @method static self IEC_62196_T2_COMBO()
  * @method static self IEC_62196_T3A()
  * @method static self IEC_62196_T3C()
+ * @method static self NEMA_5_20()
+ * @method static self NEMA_6_30()
+ * @method static self NEMA_10_30()
+ * @method static self NEMA_10_50()
+ * @method static self NEMA_14_30()
+ * @method static self NEMA_14_50()
+ * @method static self PANTOGRAPH_BOTTOM_UP()
+ * @method static self PANTOGRAPH_TOP_DOWN()
  * @method static self TESLA_R()
  * @method static self TESLA_S()
  */
@@ -48,6 +61,11 @@ class ConnectorType extends Enum
     public const DOMESTIC_J = 'DOMESTIC_J';
     public const DOMESTIC_K = 'DOMESTIC_K';
     public const DOMESTIC_L = 'DOMESTIC_L';
+    public const DOMESTIC_M = 'DOMESTIC_M';
+    public const DOMESTIC_N = 'DOMESTIC_N';
+    public const DOMESTIC_O = 'DOMESTIC_O';
+    public const GBT_AC = 'GBT_AC';
+    public const GBT_DC = 'GBT_DC';
     public const IEC_60309_2_SINGLE_16 = 'IEC_60309_2_single_16';
     public const IEC_60309_2_THREE_16 = 'IEC_60309_2_three_16';
     public const IEC_60309_2_THREE_32 = 'IEC_60309_2_three_32';
@@ -58,6 +76,14 @@ class ConnectorType extends Enum
     public const IEC_62196_T2_COMBO = 'IEC_62196_T2_COMBO';
     public const IEC_62196_T3A = 'IEC_62196_T3A';
     public const IEC_62196_T3C = 'IEC_62196_T3C';
+    public const NEMA_5_20 = 'NEMA_5_20';
+    public const NEMA_6_30 = 'NEMA_6_30';
+    public const NEMA_10_30 = 'NEMA_10_30';
+    public const NEMA_10_50 = 'NEMA_10_50';
+    public const NEMA_14_30 = 'NEMA_14_30';
+    public const NEMA_14_50 = 'NEMA_14_50';
+    public const PANTOGRAPH_BOTTOM_UP = 'PANTOGRAPH_BOTTOM_UP';
+    public const PANTOGRAPH_TOP_DOWN = 'PANTOGRAPH_TOP_DOWN';
     public const TESLA_R = 'TESLA_R';
     public const TESLA_S = 'TESLA_S';
 }
