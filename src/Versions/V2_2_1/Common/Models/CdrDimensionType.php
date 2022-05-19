b2ec0dfc3598ac656af952f101640f313a369506
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-05-20 00:19:16 +0200

    CdrDimension updated to spec

--- src/Versions/V2_2_1/Common/Models/CdrDimensionType.php
+++ src/Versions/V2_2_1/Common/Models/CdrDimensionType.php
@@ -7,19 +7,33 @@ namespace Chargemap\OCPI\Versions\V2_2_1\Common\Models;
 use MyCLabs\Enum\Enum;
 
 /**
+ * @method static self CURRENT() 
  * @method static self ENERGY()
- * @method static self FLAT()
+ * @method static self ENERGY_EXPORT()
+ * @method static self ENERGY_IMPORT()
  * @method static self MAX_CURRENT()
  * @method static self MIN_CURRENT()
+ * @method static self MAX_POWER()
+ * @method static self MIN_POWER()
  * @method static self PARKING_TIME()
+ * @method static self POWER()
+ * @method static self RESERVATION_TIME()
+ * @method static self STATE_OF_CHARGE()
  * @method static self TIME()
  */
 class CdrDimensionType extends Enum
 {
+    public const CURRENT = 'CURRENT';
     public const ENERGY = 'ENERGY';
-    public const FLAT = 'FLAT';
+    public const ENERGY_EXPORT = 'ENERGY_EXPORT';
+    public const ENERGY_IMPORT = 'ENERGY_IMPORT';
     public const MAX_CURRENT = 'MAX_CURRENT';
     public const MIN_CURRENT = 'MIN_CURRENT';
+    public const MAX_POWER = 'MAX_POWER';
+    public const MIN_POWER = 'MIN_POWER';
     public const PARKING_TIME = 'PARKING_TIME';
+    public const POWER = 'POWER';
+    public const RESERVATION_TIME = 'RESERVATION_TIME';
+    public const STATE_OF_CHARGE = 'STATE_OF_CHARGE';
     public const TIME = 'TIME';
 }
