bb26d32859d26fff50b46bc9b324574122b24fb8
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-07-23 19:56:14 +0200

    v2.1.1 Session GetListing request and response added

---
+++ src/Versions/V2_1_1/Server/Cpo/Sessions/GetListing/OcpiCpoSessionGetListingRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chargemap\OCPI\Versions\V2_1_1\Server\Cpo\Sessions\GetListing;
+
+use Chargemap\OCPI\Common\Server\OcpiPaginatedRequest;
+
+class OcpiCpoSessionGetListingRequest extends OcpiPaginatedRequest
+{
+}
