ad85e37f1f6c3741b19caaaa068dd94976ca97ef
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-08-11 23:29:14 +0200

    Commands result/request added

--- src/Versions/V2_1_1/Client/Emsp.php
+++ src/Versions/V2_1_1/Client/Emsp.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace Chargemap\OCPI\Versions\V2_1_1\Client;
 
 use Chargemap\OCPI\Common\Client\Modules\AbstractFeatures;
+use Chargemap\OCPI\Versions\V2_1_1\Client\Emsp\Commands;
 use Chargemap\OCPI\Versions\V2_1_1\Client\Versions;
 
 class Emsp extends AbstractFeatures
 {
+    private Commands $commands;
     private Versions $versions;
+
+    public function commands(): Commands
+    {
+        if(!isset($this->commands)) {
+            $this->commands = new Commands($this->ocpiConfiguration);
+        }
+
+        return $this->commands;
+    }
 
     public function versions(): Versions
     {
