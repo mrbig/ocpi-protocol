9cb55aacae99b2bbf3c6aa481a13a7975984e59e
Nagy Attila Gabor <mrbig@sneaker.hu>
2022-08-29 00:52:21 +0200

    eMSP get client added

---
+++ src/Versions/V2_1_1/Client/Emsp/Locations/Evses/Get/GetEvseService.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chargemap\OCPI\Versions\V2_1_1\Client\Emsp\Locations\Evses\Get;
+
+use Chargemap\OCPI\Common\Client\Modules\AbstractFeatures;
+
+class GetEvseService extends AbstractFeatures
+{
+    /**
+     * @param GetEvseRequest $request
+     * @return GetEvseResponse
+     * @throws \Chargemap\OCPI\Common\Client\OcpiEndpointNotFoundException
+     * @throws \Chargemap\OCPI\Common\Client\OcpiUnauthorizedException
+     * @throws \Chargemap\OCPI\Common\Server\Errors\OcpiInvalidPayloadClientError
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
+    public function handle(GetEvseRequest $request): GetEvseResponse
+    {
+        $responseInterface = $this->sendRequest($request);
+        return GetEvseResponse::from($responseInterface);
+    }
+}
