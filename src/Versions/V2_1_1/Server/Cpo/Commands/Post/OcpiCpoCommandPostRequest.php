c2b6405788a82d2440af9cd842f4803319e0b201
Nagy Attila Gabor <mrbig@sneaker.hu>
2023-01-20 01:09:44 +0100

    Fixed typo in v2.1.1 OcpiCpoCommandPostRequest

--- src/Versions/V2_1_1/Server/Cpo/Commands/Post/OcpiCpoCommandPostRequest.php
+++ src/Versions/V2_1_1/Server/Cpo/Commands/Post/OcpiCpoCommandPostRequest.php
@@ -46,7 +46,7 @@ abstract class OcpiCpoCommandPostRequest extends OcpiBaseRequest
 
     public function getCommandType(): CommandType
     {
-        return $this->commandTyoe;
+        return $this->commandType;
     }
 
     public function getCommand(): Command
