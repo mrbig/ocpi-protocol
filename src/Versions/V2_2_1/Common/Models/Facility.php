80ec03e804ba4135e2bc535d327dd209318c7db9
Roberto Chiarito <roberto.chiarito@pair.se>
2023-03-16 15:01:05 +0100

    Add missing enum values for v2.2.1

--- src/Versions/V2_2_1/Common/Models/Facility.php
+++ src/Versions/V2_2_1/Common/Models/Facility.php
@@ -16,10 +16,14 @@ use MyCLabs\Enum\Enum;
  * @method static self RECREATION_AREA()
  * @method static self NATURE()
  * @method static self MUSEUM()
+ * @method static self BIKE_SHARING()
  * @method static self BUS_STOP()
  * @method static self TAXI_STAND()
+ * @method static self TRAM_STOP()
  * @method static self TRAIN_STATION()
+ * @method static self METRO_STATION()
  * @method static self AIRPORT()
+ * @method static self PARKING_LOT()
  * @method static self CARPOOL_PARKING()
  * @method static self FUEL_STATION()
  * @method static self WIFI()
@@ -35,10 +39,14 @@ class Facility extends Enum
     public const RECREATION_AREA = 'RECREATION_AREA';
     public const NATURE = 'NATURE';
     public const MUSEUM = 'MUSEUM';
+    public const BIKE_SHARING = 'BIKE_SHARING';
     public const BUS_STOP = 'BUS_STOP';
     public const TAXI_STAND = 'TAXI_STAND';
+    public const TRAM_STOP = 'TRAM_STOP';
     public const TRAIN_STATION = 'TRAIN_STATION';
+    public const METRO_STATION = 'METRO_STATION';
     public const AIRPORT = 'AIRPORT';
+    public const PARKING_LOT = 'PARKING_LOT';
     public const CARPOOL_PARKING = 'CARPOOL_PARKING';
     public const FUEL_STATION = 'FUEL_STATION';
     public const WIFI = 'WIFI';
