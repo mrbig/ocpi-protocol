cd0af1a5f7bb78bf91f3f58ea498a7a72979f98a
Andreas Radloff <andreas.radloff@chargepanel.com>
2025-08-28 14:16:45 +0200

    added Token get listing to client

---
+++ src/Common/Client/Modules/Tokens/GetListing/GetTokensListingRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chargemap\OCPI\Common\Client\Modules\Tokens\GetListing;
+
+use Chargemap\OCPI\Common\Client\Modules\Tokens\TokensRequest;
+
+abstract class GetTokensListingRequest extends TokensRequest
+{
+}
--- src/Versions/V2_2_1/Client/Sender/Tokens.php
+++ src/Versions/V2_2_1/Client/Sender/Tokens.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Chargemap\OCPI\Versions\V2_2_1\Client\Sender;
 
 use Chargemap\OCPI\Common\Client\Modules\AbstractFeatures;
+use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\GetListing\GetTokensListingRequest;
+use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\GetListing\GetTokensListingResponse;
+use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\GetListing\GetTokensListingService;
 use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\Post\PostTokenRequest;
 use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\Post\PostTokenResponse;
 use Chargemap\OCPI\Versions\V2_2_1\Client\Sender\Tokens\Post\PostTokenService;
@@ -15,5 +18,22 @@ class Tokens extends AbstractFeatures
     public function post(PostTokenRequest $request): PostTokenResponse
     {
         return (new PostTokenService($this->ocpiConfiguration))->handle($request);
+    }
+
+    /**
+     * @param GetTokensListingRequest|null $listingRequest
+     * @return GetTokensListingResponse
+     * @throws \Chargemap\OCPI\Common\Client\OcpiEndpointNotFoundException
+     * @throws \Chargemap\OCPI\Common\Client\OcpiUnauthorizedException
+     * @throws \Chargemap\OCPI\Common\Server\Errors\OcpiInvalidPayloadClientError
+     * @throws \Psr\Http\Client\ClientExceptionInterface
+     */
+    public function getListing(?GetTokensListingRequest $listingRequest = null): GetTokensListingResponse
+    {
+        if ($listingRequest === null) {
+            $listingRequest = new GetTokensListingRequest();
+        }
+
+        return (new GetTokensListingService($this->ocpiConfiguration))->handle($listingRequest);
     }
 }
